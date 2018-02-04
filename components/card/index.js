b253cf67a46c966575eb3e47e154fffdc4d31f8e
Hiram J. Pérez <hi@worg.xyz>
2018-02-04 13:07:45 -0600

    deck detail view complete

--- components/card/index.js
+++ components/card/index.js
@@ -1,15 +1,83 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import {
+  Button,
+  Container,
+  Content,
+  H1,
+  H2,
+  View,
+  Text,
+} from 'native-base';
 
 export default class Card extends React.Component {
   static navigationOptions = ({ navigation: { state } }) => ({
-    title: state.params.deck.title,
-  })
+    headerTitle: state.params.title,
+  });
+
+  addCard = () => {
+    const deck = this.props.navigation.state.params;
+    this.props.navigation.navigate('AddCard', { id:  deck.id });
+  };
+
+  startQuiz = () => {
+    const deck = this.props.navigation.state.params;
+    this.props.navigation.navigate('Quiz', { ...deck });
+  }
 
   render() {
-    const { navigation } = this.props;
+    const deck = this.props.navigation.state.params;
+    const count = deck.questions.length;
     return (
-      <Text onPress={() => navigation.navigate('Quiz')}>{JSON.stringify(navigation)}</Text>
+      <Container>
+        <Content padder contentContainerStyle={styles.container}>
+          <H1 style={styles.heading}>
+            {deck.title}
+          </H1>
+          <H2 style={styles.count}>
+            {count} cards
+          </H2>
+          <View style={styles.buttonContainer}>
+            <Button
+              block
+              bordered
+              rounded
+              onPress={this.addCard}
+              style={styles.button}>
+              <Text>Add Card</Text>
+            </Button>
+            <Button
+              block
+              primary
+              rounded
+              disabled={count < 1}
+              onPress={this.startQuiz}
+              style={styles.button}>
+              <Text>Start Quiz</Text>
+            </Button>
+          </View>
+        </Content>
+      </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  heading: {
+    margin: 30,
+  },
+  count: {
+    margin: 20,
+    opacity: .8
+  },
+  buttonContainer: {
+    margin: 30,
+    minWidth: 300,
+  },
+  button: {
+    margin: 10,
+  }
+});
